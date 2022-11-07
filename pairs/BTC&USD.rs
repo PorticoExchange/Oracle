822dad7009811f81fead5c36402e4214a0d4eee7
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-11-07 07:05:03 -0300

    Update and rename BTC&USD to pairs/BTC&USD.rs

---
+++ pairs/BTC&USD.rs
@@ -0,0 +1,11 @@
+use super::{PriceFeed, PriceFeedError, Result};
+use crate::AssetPair;
+use async_trait::async_trait;
+use log::info;
+use reqwest::Client;
+use serde::Deserialize;
+use serde_json::Value;
+use std::collections::HashMap;
+use time::OffsetDateTime;
+
+pub struct BTC&USD
